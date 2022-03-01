4dcec6e7f62298320c5cc3bf538b776fe6d8bd3d
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-01 14:53:23 +0530

    Update config.py

--- bots/config.py
+++ bots/config.py
@@ -35,9 +35,9 @@ class Config:
     CHUNK_SIZE = 10280
     # MyTelegram.org
     # configurtion required while creating new application
-    APP_TITLE = os.environ.get("APP_TITLE", "usetgxbot")
-    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "usetgxbot")
-    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/UseTGxBot")
+    APP_TITLE = os.environ.get("APP_TITLE", "devourapibot")
+    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "devourapibot")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/devourapibot")
     # these platform informations were obtained
     # on 27.01.2020 21:15:50 IST
     APP_PLATFORM = [
@@ -52,13 +52,13 @@ class Config:
     ]
     # if any of the platform, does not work
     # please reopen
-    # https://github.com/SpEcHiDe/MyTelegramOrgRoBot/issues/3
+    # https://github.com/kamarjahan/API-DEVELOPMENT-BOT/issues/3
     APP_DESCRIPTION = os.environ.get(
         "APP_DESCRIPTION",
-        "created using https://telegram.dog/UseTGxBot"
+        "created using https://telegram.dog/devourapibot"
     )
     #
-    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @SpEcHlDe")
+    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @devourapibot")
     # the strings used in the different messages
     # in the bot
     START_TEXT = os.environ.get("START_TEXT", Translation.START_TEXT)
